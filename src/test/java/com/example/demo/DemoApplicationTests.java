0809d05fbc9ac1ba651f3ba5cb255d4c53b00985
Martín <magadiflo@gmail.com>
2022-11-11 23:55:31 -0500

    Lets Write a Test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    Calculator underTest = new Calculator();
+
+    @Test
+    void itShouldAddTwoNumbers() {
+        // Given (dado)
+        int numberOne = 20;
+        int numberTwo = 30;
+
+        // When (cuando)
+        int result = underTest.add(numberOne, numberTwo);
+
+        // Then
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    class Calculator {
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
 
 }
