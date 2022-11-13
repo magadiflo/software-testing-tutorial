ea67d688f72542400b823ad5b6ea6cc732aa0cd3
Martín <magadiflo@gmail.com>
2022-11-12 23:07:24 -0500

    Argument Captor

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class StudentServiceTest {
@@ -35,9 +38,25 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addStudent() {
+    void canAddStudent() {
+        // Given
+        Student student = new Student("Martín", "martin@gmail.com", Gender.MALE);
 
+        // When
+        this.underTest.addStudent(student);
+
+        // Then
+        // Con el ArgumentCaptor de Mockito, capturamos el argumento que llega justo dentro del
+        // método studentRepository.save(...), esto con la finalidad de comparar, es decir,
+        // tanto el valor capturado como el student definido al inicio de este test, deben
+        // ser iguales, ya que supuestamente es lo que se envió al método addStudent() de la
+        // clase StudentService
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+        Mockito.verify(this.studentRepository).save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
     }
 
     @Test
